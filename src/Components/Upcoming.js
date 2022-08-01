91494774899acc28bc91228aa15572ae1a5bb01a
MANIKANTATHOTA23 <manikantathota23@gmail.com>
2022-08-01 11:37:27 +0530

    updated Upcoming events page

--- src/Components/Upcoming.js
+++ src/Components/Upcoming.js
@@ -1,32 +1,42 @@
 import React from 'react'
 import Footer from './Footer'
-// import './WhyClub.css'
-import './WhyCards.css'
-import WhyCards from './WhyCards'
+import './Upcoming.css'
+import Upcomingcards from './Upcomingcards'
 
 function Upcoming() {
   return (
     <div className='cards'>
-      <div className='cards__container'>
+
       <h1>Upcoming Events</h1>
+        
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
 
-          <WhyCards src='https://codeforces.com/predownloaded/54/8a/548a369b1a954b2f7f9133072a025435c8667aac.png' 
-          text='Coming Event' color='#A245d9'
-          />
-
-          <WhyCards src='https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Codechef%28new%29_logo.svg/1200px-Codechef%28new%29_logo.svg.png' 
-          text='Coming Event' color='#cb5cd6'
-          />
-          
-          <WhyCards src='https://image-prod.kigyolog.com/contents/company/e25ef931a2726cc0bbbc4f41cbdb32e0.jpg' 
-          text='Coming Event' color='#dc45a3'
-          />
-          
-          <WhyCards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
-          text='Coming Event' color='#dcf563'
-          />
-          
-      </div>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
+        <Upcomingcards src='https://cdn-images-1.medium.com/max/2600/1*UGT1Rh9xLww3JeIDR1F0RQ.png' 
+        head="Name"
+        des = "Participate now" link = 'https://www.hackerrank.com/'/>
       <Footer />
     </div>
   )
